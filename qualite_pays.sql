0ecb8cb3d71ac06ed27c6d586ff1ec37ec9d36ed
Magilan ELILTHAMILVALAVAN <11400930@f207-0.ig-edu.univ-paris13.fr>
2018-10-09 16:23:30 +0200

    commentaire qualité pays

--- qualite_pays.sql
+++ qualite_pays.sql
@@ -3,3 +3,5 @@ create or replace view test(f,s,d) as select first, second, UTL_MATCH.EDIT_DISTA
 update vilpays set col1=(select f from test where s=col1) where col1=ANY(select s from test);
 
 select col1,count(*) from vilpays group by col1 order by col1;
+
+--- Nous avons choisi de ne pas faire de choix lorsque 2 pays se ressemblant ont le meme nombre d'entrées car on ne peut pas faire un choix juste parmis ces 2 pays. Nous attendrons donc d'autres insertion pour relancer ce script  
