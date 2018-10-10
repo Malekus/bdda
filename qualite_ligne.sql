59a22b0431f957cbebe10292d34b10ea0ac326c4
Magilan ELILTHAMILVALAVAN <11400930@g215-0.ig-edu.univ-paris13.fr>
2018-10-10 08:31:32 +0200

    ça marche

--- qualite_ligne.sql
+++ qualite_ligne.sql
@@ -8,7 +8,19 @@ update TABCLI set col2='MADEMOISELLE' where col2='MLLE';
 update TABCLI set col2='MONSIEUR' where col2='MR';
 update TABCLI set col2='MADAME' where col2='MME';
 
+DELETE FROM TABCLI
+               WHERE ROWID IN (
+                            SELECT ROWID
+                            FROM   ( SELECT ROWID,COL1, ROW_NUMBER ( ) OVER ( PARTITION BY (col1 || col2 || col3 || col4) ORDER BY col1 || col2 ) num_ligne
+                                    FROM   TABCLI ) t2
+                            WHERE  num_ligne >1 );
 
+SELECT ROWID,COL1, ROW_NUMBER ( ) OVER ( PARTITION BY (col1 || col2 || col3 || col4) ORDER BY col1 || col2 ) num_ligne FROM TABCLI;
+
+
+
+
+/*
 create or replace view test2() as 
 
 
@@ -18,12 +30,13 @@ create or replace view test(f,s,d) as select  first, second, UTL_MATCH.EDIT_DIST
 select col4 from tabcli where col4=(select f from test); 
 
 
-create view test3(col1,col2,col3,col4,count) as select col5 from tabcli intersect select col1,col2,col3,col4,count(*) from tabcli group by col1,col2,col3,col4 having count(*)>1 order by col1 ;
+create view test3(col1,col2,col3,col4,count) as 
+select col5 from tabcli intersect select col1,col2,col3,col4,count(*) from tabcli group by col1,col2,col3,col4 having count(*)>1 order by col1 ;
 
 select c from tabcli where col1,col2,col3,col4 in test3;
 
 select c from tabcli group by col1,col2,col3,col4;
 	select * from tabcli 
 		intersect 
-	select * from test3;
+	select * from test3;*/
 
