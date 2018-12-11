d4f879a8303b5cdf02a54dce2e6bf04a31426306
Magilan ELILTHAMILVALAVAN <11400930@g215-0.ig-edu.univ-paris13.fr>
2018-12-11 13:24:43 +0100

    ??

--- titi.sql
+++ titi.sql
@@ -1,9 +1,12 @@
 create table t5 (id number(10),ville varchar(100), pays varchar(100));
 insert into t5 values(1,'paris','FRANCE');
-insert into t5 values(2,'lyon','FRANCE');
+insert into t5 values(2,'lyon','FRANC');
 insert into t5 values(3,'paris','');
-insert into t5 values(4,'Sète','FRANCE');
+insert into t5 values(4,'Three Legged Cross','FRANCE');
 insert into t5 values(5,'Nottingham','Royaume-Uni');
 insert into t5 values(6,'Latina','Espagne');
 insert into t5 values(7,'Teo','ESPAGNE');
-
+insert into t5 values(8,'bordeau','Italie');
+insert into t5 values(9,'VALENCIA','SPAIN');
+insert into t5 values(10,'VALENCIA','SPAIN');
+insert into t5 values(11,'VALENCE','SPAIN');
