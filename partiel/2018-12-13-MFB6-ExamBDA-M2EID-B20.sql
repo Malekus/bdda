7029ed23dd57ebbf0eb12c6c7ec25b13e31a5d88
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-12-13 13:53:50 +0100

    update

--- partiel/2018-12-13-MFB6-ExamBDA-M2EID-B20.sql
+++ partiel/2018-12-13-MFB6-ExamBDA-M2EID-B20.sql
@@ -1,12 +1,12 @@
 --====  FB ===========================================================================
 --====  FB ===========================================================================
--- Université Sorbonne Paris Cité - Promotion 2018-2019 - Décembre 2018
+-- Universite Sorbonne Paris Cite - Promotion 2018-2019 - Decembre 2018
 --====  FB ===========================================================================
 
 -- Binôme Bxy :  ELILTHAMILVALAVAN Magilan            *** ELILTHAMILVALAVAN Magilan
 
--- Master 2 Informatique, Professionnel, Exploration Informatique des Données et Décisionnel
--- Examen de Bases de Données Avancées
+-- Master 2 Informatique, Professionnel, Exploration Informatique des Donnees et Decisionnel
+-- Examen de Bases de Donnees Avancees
 --====  FB ===========================================================================
 
 -- Nouveau nom du Fichier SQL : 2018-Profiler-B20.sql
@@ -16,25 +16,25 @@
 
 
 /*
-M. F. Boufarès ; 
-Université Sorbonne Paris Cité
-Université Paris 13 ; Master 2 EID2
-Jeudi 13 décembre 2018
+M. F. Boufares ; 
+Universite Sorbonne Paris Cite
+Universite Paris 13 ; Master 2 EID2
+Jeudi 13 decembre 2018
 
-Vous êtes le TOUBIB-TABIB des données : Un Data-Logue 
+Vous êtes le TOUBIB-TABIB des donnees : Un Data-Logue 
 (Comme par exemple les Cardio-logues, les Pneumo-logues, les Uro-logues,...) !
 
-Vous disposez des outils qui vous permettent de DIAGNOSTIQUER les anomalies de votre patient la source de données DataSource !
+Vous disposez des outils qui vous permettent de DIAGNOSTIQUER les anomalies de votre patient la source de donnees DataSource !
 
 Est-ce une mission possible ?! SiSi C PO CIBLE !!!
 VOTRE MISSION SI VOUS L'ACCEPTEZ EST DE DONNER UN SENS AUX DONNEES !
 SI VOUS ECHOUEZ, NOUS NIERONS AVOIR EU CONNAISSANCE DE VOS AGISSEMENTS !
 
 Faites le diagnostic automatiquement qui vous permet :
-- de détecter les erreurs (les anomalies), et ensuite,
+- de detecter les erreurs (les anomalies), et ensuite,
 - de corriger les anomalies
 
-Vous êtes entrain de développer un outil de qualité très intelligent : --->>>>>> SmartDATA
+Vous êtes entrain de developper un outil de qualite tres intelligent : --->>>>>> SmartDATA
 Est-ce une mission impossible ?! SiSi C PO CIBLE !!!
 
 -----??????????? V -----??????????? *******************************
@@ -64,19 +64,19 @@ CREATE TABLE DATASOURCE ( NOM VARCHAR2(20), PRENOM VARCHAR2(20), DATNAISS VARCHA
 INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22/02/2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69Kg', '+33 7 77 77 77 77', 'lina.lameme@gmail.com');
 INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Adam', '10/06/1996', 'Paris', 'France', 'M', 'B+', '172cm', '71', '+33617716698', 'adam.clement@gmail.com');
 INSERT INTO DATASOURCE VALUES ('LABELLE', 'Eve', '17/06/1990', 'Paris', 'Fr', 'F', 'B', '169cm', '', '669964916', 'eve.la belle@gmail.com');
-INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clémence', '01/10/1920', 'Marseille', 'France', 'F', 'A+', '1,68m', '68kg', '684071896', 'clémence.clement@gmail.com');
+INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clemence', '01/10/1920', 'Marseille', 'France', 'F', 'A+', '1,68m', '68kg', '684071896', 'clemence.clement@gmail.com');
 INSERT INTO DATASOURCE VALUES ('TRAIFOR', 'Adam', '19/06/2001', 'Lyon', 'France', 'M', 'B+', '1700mm', '71kg', '(+33) 06 30 50 19 16', 'adam.traifor@gmail.com');
 INSERT INTO DATASOURCE VALUES ('EVE', 'Evelyne', '22 novembre 1969', '-', '', 'F', '', '', '', '687844442', 'evelyne!?/eve@gmail.com');
-INSERT INTO DATASOURCE VALUES ('NANNOU', 'Inès', '22 novembre 1969', 'Nice', 'France', 'F', 'B+', '1,69m', '70KG', '678466837', 'ines.nan@nou@gmail.com');
+INSERT INTO DATASOURCE VALUES ('NANNOU', 'Ines', '22 novembre 1969', 'Nice', 'France', 'F', 'B+', '1,69m', '70KG', '678466837', 'ines.nan@nou@gmail.com');
 INSERT INTO DATASOURCE VALUES ('GRAND', 'Adam', '16 octobre 1996', 'Paris', '', 'M', 'B+', '1920mm', '71KiloG', '646532809', 'adam.grand@gmail.com');
-INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 février 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '+33 7 77 77 77 77', 'lina.lameme@gmail.com');
-INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 février 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '+33 7 77 77 77 77', '');
-INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 février 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '', 'lina.lameme@gmail.com');
-INSERT INTO DATASOURCE VALUES ('LAMEME', 'L.', '22 février 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '', 'lina.lameme@gmail.com');
-INSERT INTO DATASOURCE VALUES ('lameme', 'lina', '22 février 2002', 'lille', '', 'f', '', '155cm', '69kg', '+33 7 77 77 77 77', 'lina.lameme@gmail.com');
-INSERT INTO DATASOURCE VALUES ('lameme', 'lina', '22 février 2002', 'lille', 'franc', 'f', 'ab+', '155cm', '69kg', '+33 7 77 77 77 77', 'lina.lameme@gmail');
-INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clémence', '11 novembre 2011', 'Barcelone', 'Espagne', 'F', 'A+', '111cm', '13kg', '', 'fcb-clement@yahoo.fr');
-INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clémence', '11 novembre 2011', 'Barcelone', 'Espagne', 'F', 'A+', '1,11m', '13000g', '', 'fcb-clement@yahoo.fr');
+INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 fevrier 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '+33 7 77 77 77 77', 'lina.lameme@gmail.com');
+INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 fevrier 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '+33 7 77 77 77 77', '');
+INSERT INTO DATASOURCE VALUES ('LAMEME', 'Lina', '22 fevrier 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '', 'lina.lameme@gmail.com');
+INSERT INTO DATASOURCE VALUES ('LAMEME', 'L.', '22 fevrier 2002', 'Lille', 'France', 'F', 'AB+', '155cm', '69KG', '', 'lina.lameme@gmail.com');
+INSERT INTO DATASOURCE VALUES ('lameme', 'lina', '22 fevrier 2002', 'lille', '', 'f', '', '155cm', '69kg', '+33 7 77 77 77 77', 'lina.lameme@gmail.com');
+INSERT INTO DATASOURCE VALUES ('lameme', 'lina', '22 fevrier 2002', 'lille', 'franc', 'f', 'ab+', '155cm', '69kg', '+33 7 77 77 77 77', 'lina.lameme@gmail');
+INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clemence', '11 novembre 2011', 'Barcelone', 'Espagne', 'F', 'A+', '111cm', '13kg', '', 'fcb-clement@yahoo.fr');
+INSERT INTO DATASOURCE VALUES ('CLEMENT', 'Clemence', '11 novembre 2011', 'Barcelone', 'Espagne', 'F', 'A+', '1,11m', '13000g', '', 'fcb-clement@yahoo.fr');
 INSERT INTO DATASOURCE VALUES ('CLEMENT', 'clemence', '2011-novembre-11', 'Barcelone', 'Espagne', 'F', 'A+', '1,11m', '13000g', '', 'fcb-clement@yahoo.fr');
 COMMIT;
 
@@ -89,7 +89,7 @@ SELECT DISTINCT NOM FROM DATASOURCE;
 SELECT DISTINCT UPPER(NOM) FROM DATASOURCE;
 SELECT EMAIL FROM DATASOURCE;
 
--- Homogénéisation !!! Comment !
+-- Homogeneisation !!! Comment !
 
 SELECT TAILLE FROM DATASOURCE;
 SELECT AVG(TAILLE) AS MOYTAIL FROM DATASOURCE ;
@@ -97,36 +97,36 @@ SELECT MAX(POIDS) AS MAXPDS FROM DATASOURCE ;
 
 --====  2 ===========================================================================
 
--- Il y aurait des problèmes dans cette source de données !!
+-- Il y aurait des problemes dans cette source de donnees !!
 
 REM Que faire ?
 REM C EST SMARTDATA QUI VA NOUS RENSEIGNER !
 
--- Développez le mécanisme qui permet de diagnostiquer (profiler) les problèmes dans la source de données
--- Complétez/Corrigez le script sql 2018ProfilerDonnees.sql --==>>> 2018-12-13-Profiler-Bxy.sql
+-- Developpez le mecanisme qui permet de diagnostiquer (profiler) les problemes dans la source de donnees
+-- Completez/Corrigez le script sql 2018ProfilerDonnees.sql --==>>> 2018-12-13-Profiler-Bxy.sql
 
 --====  3 ===========================================================================
 
--- Dans l'exemble d'application traité en cours-TD-TP "GEStion COMmerciale"
--- Les villes & les pays des clients et des magasins peuvent consituer de nouvelles données 
--- qui permettraient d'enrichir le dicionnaire de données DDVS (Data Dictionnary for Valid Strings) utilisé pour corriger des anomalies
--- Proposez une métthode d'enrichissement de ce dictionnaire et développez le script sql correspondant
+-- Dans l'exemble d'application traite en cours-TD-TP "GEStion COMmerciale"
+-- Les villes & les pays des clients et des magasins peuvent consituer de nouvelles donnees 
+-- qui permettraient d'enrichir le dicionnaire de donnees DDVS (Data Dictionnary for Valid Strings) utilise pour corriger des anomalies
+-- Proposez une metthode d'enrichissement de ce dictionnaire et developpez le script sql correspondant
 
 --====  4 ===========================================================================
 
--- Dans l'exemble d'application traité en cours-TD-TP "GEStion COMmerciale"
--- Le schéma, relationnel, de la BD GESCOM 
--- est composé de plusieurs tables et leurs dérivés : CLIENTS, COMMANDES, ARTICLES, DETAILSCOM, MAGASINS...
+-- Dans l'exemble d'application traite en cours-TD-TP "GEStion COMmerciale"
+-- Le schema, relationnel, de la BD GESCOM 
+-- est compose de plusieurs tables et leurs derives : CLIENTS, COMMANDES, ARTICLES, DETAILSCOM, MAGASINS...
 
--- Détaillez la manière de définir un nouveau schéma cible-destination (dit schéma en étoile) extrait depuis toutes les sources
--- Celui-ci permet de faire des croisements-analyses-extractions-exploitations des données tel que celui des ventes par rapport 
+-- Detaillez la maniere de definir un nouveau schema cible-destination (dit schema en etoile) extrait depuis toutes les sources
+-- Celui-ci permet de faire des croisements-analyses-extractions-exploitations des donnees tel que celui des ventes par rapport 
 -- aux articles, aux magasins, aux clients et à la dimension temps...
 
--- Comparez le avec le schéma global qui regroupe toutes les tables (l'Union-l'Intégration de toutes les tables de toutes les BD)
+-- Comparez le avec le schema global qui regroupe toutes les tables (l'Union-l'Integration de toutes les tables de toutes les BD)
 
 --====  5 ===========================================================================
 
--- N'oubliez pas, si vous le souhaitez, d'envoyer la version définitive (G-Bxy) de tous vos travaux avant le jj-décembre 2018
+-- N'oubliez pas, si vous le souhaitez, d'envoyer la version definitive (G-Bxy) de tous vos travaux avant le jj-decembre 2018
 
 --====  FB ===========================================================================
 --====  FB ===========================================================================
--- partiel/rendu.sql
+++ partiel/rendu.sql
@@ -108,7 +108,6 @@ END;
 
 
 
-/*
 DROP TABLE DDRE;
 
 CREATE TABLE DDRE 
@@ -125,37 +124,13 @@ INSERT INTO DDRE VALUES ('WEIGHT','WEIGHT-KG','^([1-9]([0-9])*(,[1-9])?)( )?([kK
 INSERT INTO DDRE VALUES ('SIZE','SIZE-MM','^([1-9]([0-9])*(,[1-9])?)( )?([mM]{2})$','SIZE001',''); 
 INSERT INTO DDRE VALUES ('SIZE','SIZE-CM','^([1-9]([0-9])*(,[1-9])?)( )?([cC][mM])$','SIZE002',''); 
 INSERT INTO DDRE VALUES ('SIZE','SIZE-M','^([1-9]([0-9])*(,[1-9])?)( )?([mM])$','SIZE003',''); 
-
 INSERT INTO DDRE VALUES ('GS','GRSANG','^(A|B|O|AB)(\+|\-)$','GR001',''); 
-
-
- -- poids: exemple 3lb, tonne, once, pounds
-INSERT INTO DDRE VALUES ('WEIGHT','WEIGHT-TONNE','^[0-9]+[[:space:]]?(lb|oz|livre|once|tonne|t|pounds)$','WEIGHT002','');
 
 -- longueur: exemple 3m50 3,5m, cm, dam
 INSERT INTO DDRE VALUES ('LENGTH','LENGTH-M','^[0-9]+[[:space:]]?([dDcCkKhHmMnNÂµ]m|m|dam)[0-9]*$','LENGTH001','');
 
 -- longueur: exemple 45 pouce
 INSERT INTO DDRE VALUES ('LENGTH','LENGTH-POUCE','^[0-9]+[[:space:]]?(inch|pouce|pouces|foot|pied|yard|mille|mi)[0-9]*$','LENGTH002','');
-
--- vitesse: exemple 150 m/s
-INSERT INTO DDRE VALUES ('SPEED','SPEED-M/s','^[0-9]+[[:space:]]?(cm|m|ft|p)\/s$','SPEED001','');
- 
--- vitesse: exemple 150 km/h
-INSERT INTO DDRE VALUES ('SPEED','SPEED-KM/H','^[0-9]+[[:space:]]?(km|mille|mi)\/h$','SPEED002',''); 
-
--- vitesse: exemple 150 noeuds
-INSERT INTO DDRE VALUES ('SPEED','SPEED-NOEUDS','^[0-9]+[[:space:]]?(noeud|noeuds|nd|kt|kn|kts)$','SPEED003','');
-
-INSERT INTO DDRE VALUES ('TEMPERATURE','TEMPERATURE-Celsius', '[0-9]+[[:space:]]?(°C|Celsius)$','TEMPERATURE001','');
-
-INSERT INTO DDRE VALUES ('TEMPERATURE','TEMPERATURE-Fahrenheit', '[0-9]+[[:space:]]?(°F|Fahrenheit)$','TEMPERATURE002','');
-
-INSERT INTO DDRE VALUES ('TEMPERATURE','TEMPERATURE-Kelvin', '[0-9]+[[:space:]]?(K|Kelvin)$','TEMPERATURE003','');
-
-INSERT INTO DDRE VALUES ('VOLUME','','^[0-9]+[[:space:]]?([dcmhknÂµ]l|litre|gallon|gal)[0-9]*$','VOLUME001','');
-
-INSERT INTO DDRE VALUES ('URL', '','^((http|https):\/\/)?(www[.])?([a-zA-Z0-9]|-)+([.][a-zA-Z(-|\/|=|?)?]+)+$', 'URL001', '') ;
 
 INSERT INTO DDRE VALUES ('EMAIL','','^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$','EMAIL001', '') ;
 
@@ -164,8 +139,6 @@ INSERT INTO DDRE VALUES ('DATE','DATEDDMMYYYY','^(0?[1-9]|[12][0-9]|3[01])[\/\-\
 INSERT INTO DDRE VALUES ('DATE','DATEMMDDYYYY','^(0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])[\/\-\.]\d{4}$','DATE002',''); 
 
 INSERT INTO DDRE VALUES ('DATE','DATEYYYYMMDD','^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$','DATE003','');
-
-INSERT INTO DDRE VALUES ('INTEGER','','^[[:digit:]]+$','INTEGER001','');
 
 INSERT INTO DDRE VALUES ('MONEY','EURO','^([0-9]+)(.[0-9]{1,2})? (\€)$','MONEY001','');
 
@@ -186,4 +159,3 @@ INSERT INTO DDRE VALUES ('IDENTITY', 'ID', '^([0-9a-zA-Z]\1){2}([a-zA-Z0-9.])*([
 INSERT INTO DDRE VALUES ('TEL', 'TELINDI', '^(\+[1-9])([0-9]{1,2})?([0-9]{9})$', 'TELINDICATEUR001','');
 
 COMMIT;
-*/
